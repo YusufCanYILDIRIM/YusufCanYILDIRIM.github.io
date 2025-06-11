da2bc1d6c363f17efb06105110121a90946a91ea
Yusuf Can YILDIRIM <yusufyildirimcan@outlook.com>
2025-06-11 14:52:23 +0300

    Refactor code structure for improved readability and maintainability

--- database/seeders/ProfileSeeder.php
+++ database/seeders/ProfileSeeder.php
@@ -9,18 +9,20 @@ class ProfileSeeder extends Seeder
 {
     public function run(): void
     {
-        Profile::create([
-            'name' => 'Yusuf Can YILDIRIM',
-            'title' => 'Software Engineer',
-            'email' => 'yusufyildirimcan@outlook.com',
-            'phone' => '05395804157',
-            'address' => '123 Main Street',
-            'about' => 'I am a backend developer',
-            'photo' => 'template/assets/img/profile.jpg',
-            'linkedin' => 'https://www.linkedin.com/in/yusuf-can-yildirim-99490423a/',
-            'github' => 'https://github.com/YusufCanYILDIRIM',
-            'twitter' => 'https://x.com/Yusuffcann10',
-            'instagram' => 'https://www.instagram.com/yusuffcann10/',
-        ]);
+        Profile::firstOrCreate(
+            ['email' => 'yusufyildirimcan@outlook.com'], // benzersiz bir alan
+            [
+                'name' => 'Yusuf Can YILDIRIM',
+                'title' => 'Software Engineer',
+                'phone' => '05395804157',
+                'address' => '123 Main Street',
+                'about' => 'I am a backend developer',
+                'photo' => 'tamplete/images/yusuf.jpeg',
+                'linkedin' => 'https://www.linkedin.com/in/yusuf-can-yildirim-99490423a/',
+                'github' => 'https://github.com/YusufCanYILDIRIM',
+                'twitter' => 'https://x.com/Yusuffcann10',
+                'instagram' => 'https://www.instagram.com/yusuffcann10/',
+            ]
+        );
     }
 }
